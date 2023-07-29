7a013e4bab357abeb92aa8a35feb400b3e66539f
khansajeel <isajeelkhan@gmail.com>
2023-07-29 16:24:32 +0530

    second code

---
+++ codes/002.js
@@ -0,0 +1,10 @@
+console.log("This repl contains code for the Ultimate JavaScript course video no 2")
+var a = 67 // a contains 67
+console.log(a)
+a = "harry"
+console.log(a)
+//this is allowed at runtime, like python
+
+
+// let 8harry = 7 // Not allowed this will throw an error(number startnig)
+// let var = 7 // rrserved variable names
