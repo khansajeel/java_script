18dabc71270b0ed30c560ca17364a3583c1dc371
khansajeel <isajeelkhan@gmail.com>
2023-07-31 10:00:34 +0530

    operators

---
+++ codes/005_operators.js
@@ -0,0 +1,46 @@
+
+console.log("Operators in Js")
+// Arithmetic Operators 
+let a = 10;
+let b = 4;
+console.log("a + b = ", a + b)
+console.log("a - b = ", a - b)
+console.log("a / b = ", a / b)
+console.log("a ** b = ", a ** b)
+console.log("a % b = ", a % b)
+console.log("++a = ", ++a)
+console.log("a++ = ", a++)
+console.log("--a = ", --a)
+console.log("a-- = ", a--)
+console.log("a = ", a)
+console.log("a-- = ", a--)
+
+// 11 11 11 11 10 10
+
+// Assignment Operators 
+let assignment = 1;
+assignment += 5 // same as assignment = assignment + 5
+console.log("a is now = ", a)
+assignment -= 5 // same as assignment = assignment - 5
+console.log("a is now = ", a)
+assignment *= 5 // same as assignment = assignment * 5
+console.log("a is now = ", a)
+assignment /= 5 // same as assignment = assignment / 5
+console.log("a is now = ", a)
+
+// Comparison Operators 
+let comp1 = 6;
+let comp2 = 7;
+console.log("comp1 == comp2 is ", comp1 == comp2)
+console.log("comp1 != comp2 is ", comp1 != comp2)
+console.log("comp1 === comp2 is ", comp1 === comp2)
+console.log("comp1 !== comp2 is ", comp1 !== comp2)
+console.log("comp1 > comp2 is ", comp1 > comp2)
+
+// Logical Operators
+let x = 5;
+let y = 6;
+console.log(x<y && x==5)
+console.log(x>y || x==5)
+console.log(!false)
+console.log(!true)
