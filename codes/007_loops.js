4dc64f7ce5814252cba9a9e814b23de04da0fbdc
khansajeel <isajeelkhan@gmail.com>
2023-08-07 07:59:57 +0530

    loops

---
+++ codes/007_loops.js
@@ -0,0 +1,30 @@
+let a = 10;
+for (let i = 0;i<a;i++){
+    console.log(i);
+}
+
+
+let obj = {
+  sajeel : 2,
+  sajawal : 2,
+  lui : 3,
+  tahira : 41
+}
+
+//for in loop
+for (let k in obj){
+  console.log(k +"s value is:- " + obj[k]);
+}
+
+//for of loop
+for( let j of "SAJEEL"){
+  console.log(j);
+}
+
+//while
+let n = 10;
+let o = 0;
+while(o<n){
+  console.log(o++);
+}
+
